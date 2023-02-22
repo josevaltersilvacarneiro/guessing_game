017fe498fba866544202ee13df0d4a6617971423
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-02-22 19:45:39 -0300

    Continuing

--- guessing-game.cpp
+++ guessing-game.cpp
@@ -13,7 +13,9 @@ print_header(void)
 int
 main(void)
 {
+	int secret_number = 42;
+
 	print_header();                           /* Print the header */
-	
+	cout << "The value of the secret number is " << secret_number << endl;
 	return 0;
 }
