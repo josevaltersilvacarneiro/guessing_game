5f61fac8cdd64fa3643468f34c476fe680911dc4
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-02-22 19:53:31 -0300

    Allowing the user to type data

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -14,8 +14,18 @@ int
 main(void)
 {
 	int secret_number = 42;
+	int guess;
 
 	print_header();                           /* Print the header */
+
+	cout << "Type your guess: ";
+	cin >> guess;
+
+	if (secret_number != guess)
+		cout << "You lost" << endl;
+	else
+		cout << "You got it right" << endl;
+
 	cout << "The value of the secret number is " << secret_number << endl;
 	return 0;
 }
